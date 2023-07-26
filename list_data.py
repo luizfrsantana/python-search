f0e023fb5f51412b65ba9b86fb93cc14eec4cbed
luizfrsantana <luizfrsantana@gmail.com>
2023-07-26 00:04:23 -0700

    Ip teste

--- list_data.py
+++ list_data.py
@@ -8,6 +8,7 @@ for name in customer_names:
     customer_vlan = cp.parseCustomerVlan(name)
     customer_ip = cp.parseCustomerIPAddress(customer_vlan)
 
-    print(name + " - VLAN: " + str(customer_vlan) + " - IP Address: " + customer_ip + " IP Correto: " + is_ip_address.is_ip_address(customer_ip) + " IP na Rede Correta: " + is_ip_address.is_ip_correct_network(customer_ip))
+    print(name + " - VLAN: " + str(customer_vlan) + " - IP Address: " + customer_ip + 
+          " - IP Correto: " + is_ip_address.is_ip_address(customer_ip) + " - IP na Rede Correta: " + is_ip_address.is_ip_correct_network(customer_ip))
 
 
