20f6694df8464fef1fcfa49993e25d735b814733
luizfrsantana <luizfrsantana@gmail.com>
2023-07-26 00:15:30 -0700

    Convertendo a class Ip teste para função

--- list_data.py
+++ list_data.py
@@ -1,5 +1,6 @@
+import ip_test
 from extract_customer_data import ConfigurationParser
-from ip_test import is_ip_address
+
 
 cp = ConfigurationParser()
 customer_names = cp.parseCustomerNames()
@@ -9,6 +10,6 @@ for name in customer_names:
     customer_ip = cp.parseCustomerIPAddress(customer_vlan)
 
     print(name + " - VLAN: " + str(customer_vlan) + " - IP Address: " + customer_ip + 
-          " - IP Correto: " + is_ip_address.is_ip_address(customer_ip) + " - IP na Rede Correta: " + is_ip_address.is_ip_correct_network(customer_ip))
+          " - IP Correto: " + ip_test.is_ip_address(customer_ip) + " - IP na Rede Correta: " + ip_test.is_ip_correct_network(customer_ip))
 
 
